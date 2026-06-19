29e64084b718dfe4707d0cb12758210519100f56
Yanren <hi@imyan.ren>
2026-06-20 00:01:00 +0800

    feat: 吃什么

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,13 @@ import { Bot } from 'grammy';
 export function createBot(token: string) {
   const bot = new Bot(token);
 
+  bot.on('message:text', async (ctx) => {
+    const text = ctx.message.text.replace(/[\s\p{P}]+$/u, '');
+    if (text.endsWith('吃什么')) {
+      await ctx.reply('对啊，吃什么');
+    }
+  });
+
   bot.on('message:sticker', async (ctx) => {
     const stickerId = ctx.message.sticker.file_unique_id;
     // 回复爱音唐笑
